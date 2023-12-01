9b0135f74652b42585595e0816d6b881a46f4342
mrfavi <yulieko@favourse.com>
2023-12-01 15:16:33 +0800

    Change the Network

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const LOCAL_API_URL = "http://localhost:3040";
-export const LIVE_API_URL = "http://54.209.22.141:3040";
+export const LIVE_API_URL = "http://localhost:3040";
+export const LOCAL_API_URL = "https://codexdev.tech/fav";
